bbb1337d323bb369ee35aa4b9e5db00bb7c5b6d7
silent <silent161192@gmail.com>
2017-08-26 19:28:26 +0300

    char

--- perem.c
+++ perem.c
@@ -4,5 +4,11 @@ int main()
 	int MyVar = 10;
 	int Number; //Неинициализирована(без значения)
 	Number = 50;
-
+	int var, perem, folder;
+	/*
+	комментарий на разных
+	строках
+	*/
+	char letter ='S';
+	int number1 = 88;
 }
