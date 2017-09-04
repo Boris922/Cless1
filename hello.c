0287864da038c37d62b56897f38513ce342da188
silent <silent161192@gmail.com>
2017-09-04 06:26:35 +0300

    read

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //Включение информации о стандартной библиотеке
+int main()// Определение футкции без аргументов
+{
+	printf("Hello\n");
+	printf("world");
+	printf("Oyeah!!!s\n");
+}
